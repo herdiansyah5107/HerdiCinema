bc97ab2faace8e1e35a940dbe01e721a7c6d92bd
herdiansyah5107@gmail <herdiansyah5107@gmail.com>
2021-12-26 15:25:51 +0700

    Pertama

---
+++ app/src/main/java/com/example/herdi_cinema/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.herdi_cinema;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+
+import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.ShareActionProvider;
+import android.support.v4.view.MenuItemCompat;
+import com.example.bitsandpizza.R;
+
+
+public class MainActivity extends AppCompatActivity{
+    private ShareActionProvider shareActionProvider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the app bar.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        shareActionProvider =
+                (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        setShareActionIntent("Want to join me for pizza?");
+        return super.onCreateOptionsMenu(menu);
+    }
+    private void setShareActionIntent(String text) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        shareActionProvider.setShareIntent(intent);
+    }
+
+}
---
+++ app/src/main/java/com/example/herdi_cinema/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.example.herdi_cinema;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.bitsandpizza.R;
+
+
+public class SplashScreen extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    //This method will be executed once the timer is over
+                    // Start your app main activity
+                    Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(i);
+                    // close this activity
+                    finish();
+                }
+            }, 3000);
+        }
+    }
